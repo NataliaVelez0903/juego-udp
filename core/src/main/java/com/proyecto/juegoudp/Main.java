f5eebe1ccbc71a349e9065496bd2530f6ea0e5ff
Natalia Velez <natalia.velez.orjuela@gmail.com>
2026-03-30 23:57:57 -0500

    feat: crear tablero base del juego

--- core/src/main/java/com/proyecto/juegoudp/Main.java
+++ core/src/main/java/com/proyecto/juegoudp/Main.java
@@ -4,24 +4,35 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
+    private ShapeRenderer shapeRenderer;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         image = new Texture("libgdx.png");
+        shapeRenderer = new ShapeRenderer();
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0f, 0f, 1f, 1f);
-        batch.begin();
-        batch.draw(image, 50, 50);
-        batch.end();
+        ScreenUtils.clear(0f, 0.2f, 0.1f, 1f);
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        // Zona jugador 1 (izquierda)
+        shapeRenderer.setColor(0, 0, 1, 1);
+        shapeRenderer.rect(0, 0, 200, 480);
+        // Zona jugador 2 (derecha)
+        shapeRenderer.setColor(1, 0, 0, 1);
+        shapeRenderer.rect(600, 0, 200, 480);
+        // Zona central
+        shapeRenderer.setColor(0.2f, 0.2f, 0.2f, 1);
+        shapeRenderer.rect(200, 0, 400, 480);
+        shapeRenderer.end();
     }
 
     @Override
