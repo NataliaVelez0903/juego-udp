ff1455bd4c9f6567a683a2f0621be1a5d3d292c8
Natalia Velez <natalia.velez.orjuela@gmail.com>
2026-04-01 21:48:15 -0500

    este es un ensayo para sebas

--- core/src/main/java/com/proyecto/juegoudp/entrada/ControladorEntrada.java
+++ core/src/main/java/com/proyecto/juegoudp/entrada/ControladorEntrada.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Vector3;
 
-
+// Esta es la clase controladorEntrada
 public class ControladorEntrada {
 
 }
