56fa92117c63a19e5bb977e4c739d78006999c53
Luis Carlos <luiscarlosgallegomorales94@gmail.com>
2026-04-10 01:34:39 -0500

    Documentacion completa de la clase Lwjgl3Launcher que inicia el juego

--- lwjgl3/src/main/java/com/proyecto/juegoudp/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/proyecto/juegoudp/lwjgl3/Lwjgl3Launcher.java
@@ -3,10 +3,32 @@ package com.proyecto.juegoudp.lwjgl3;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.proyecto.juegoudp.JuegoPrincipal;
+
 /**
- * Punto de entrada del escritorio: ventana redimensionable; el juego escala el mundo 1024×768 al maximizar o a pantalla completa.
+ * Representa el punto de entrada de la aplicación en entorno de escritorio.
+ *
+ * Esta clase se encarga de crear y configurar la ventana principal del juego
+ * cuando se ejecuta en la plataforma LWJGL3.
+ *
+ * Aquí se definen propiedades iniciales como el título de la ventana,
+ * el tamaño inicial, la posibilidad de redimensionarla, la sincronización
+ * vertical y la cantidad máxima de fotogramas por segundo.
+ *
+ * Una vez aplicada la configuración, se crea la instancia principal del juego.
+ *
+ * @author Luis
+ * @version 1.0
  */
 public class Lwjgl3Launcher {
+
+    /**
+     * Inicia la ejecución del juego en modo escritorio.
+     *
+     * Este método configura la ventana principal y lanza la aplicación
+     * utilizando la clase principal del juego.
+     *
+     * @param args argumentos de línea de comandos recibidos al iniciar la aplicación
+     */
     public static void main(String[] args) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
         config.setTitle("Peloteros - Multijugador UDP");
